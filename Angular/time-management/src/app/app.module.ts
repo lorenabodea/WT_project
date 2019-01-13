cf5dea993b29d5dabc8adab881d87bbb462327a0
Bianca Botezatu <botezatubianca20@gmail.com>
2019-01-13 02:16:07 +0200

    components

--- Angular/time-management/src/app/app.module.ts
+++ Angular/time-management/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
